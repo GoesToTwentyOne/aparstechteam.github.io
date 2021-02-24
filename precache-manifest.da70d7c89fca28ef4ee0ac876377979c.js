ca12fa3a63544cb413ccba1929acf32e18e078d6
Nasimul Hasan Deep <45994595+nasimulhasan513@users.noreply.github.com>
2021-02-24 16:09:24 +0600

    updated

---
+++ precache-manifest.da70d7c89fca28ef4ee0ac876377979c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "571bdfd7900508cfb321",
+    "url": "/css/app.04e68da3.css"
+  },
+  {
+    "revision": "5bcc91ffcff2ad6aa97e",
+    "url": "/css/chunk-5bcd3558.57d4009d.css"
+  },
+  {
+    "revision": "671495f1d38c1235b978",
+    "url": "/css/chunk-7f2d6097.4d070b8f.css"
+  },
+  {
+    "revision": "bf0369243147bb204d95",
+    "url": "/css/chunk-8514fb96.00bbf5ee.css"
+  },
+  {
+    "revision": "61b6341cdbd380ea3f34",
+    "url": "/css/chunk-vendors.e4bfab32.css"
+  },
+  {
+    "revision": "881563d6444b370fa4ceea0c3183bb4c",
+    "url": "/img/bg.881563d6.gif"
+  },
+  {
+    "revision": "a6a7af386d199f8dd77ff7315e737410",
+    "url": "/img/techteam.a6a7af38.svg"
+  },
+  {
+    "revision": "c13f764038c8a2fccffd4958610ac17b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10a7e8da03cd6bbd5ddd",
+    "url": "/js/about.9781319d.js"
+  },
+  {
+    "revision": "571bdfd7900508cfb321",
+    "url": "/js/app.781ea9df.js"
+  },
+  {
+    "revision": "5bcc91ffcff2ad6aa97e",
+    "url": "/js/chunk-5bcd3558.c9e47cb5.js"
+  },
+  {
+    "revision": "671495f1d38c1235b978",
+    "url": "/js/chunk-7f2d6097.12eee4d3.js"
+  },
+  {
+    "revision": "bf0369243147bb204d95",
+    "url": "/js/chunk-8514fb96.368e6732.js"
+  },
+  {
+    "revision": "61b6341cdbd380ea3f34",
+    "url": "/js/chunk-vendors.9d282a36.js"
+  },
+  {
+    "revision": "9437b48e9b82f7fffef42cfd997af3f9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "890bd90090fbcd66a5d08d8c386c9b11",
+    "url": "/techteam.png"
+  }
+]);
